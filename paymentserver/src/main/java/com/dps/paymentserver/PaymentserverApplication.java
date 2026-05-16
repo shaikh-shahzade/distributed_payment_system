921cbd917f9ae2ae20fcd5f01d70e4447356c781
shaikh shahzade <shahzade10@gmail.com>
2026-05-17 00:15:55 +0530

    Payment server

---
+++ paymentserver/src/main/java/com/dps/paymentserver/PaymentserverApplication.java
@@ -0,0 +1,13 @@
+package com.dps.paymentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentserverApplication.class, args);
+	}
+
+}
