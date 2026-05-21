1c3046565255544fc27bffa7f94273a17a1c7863
shaikh shahzade <shahzade10@gmail.com>
2026-05-21 23:27:00 +0530

    Payment-executor  service initialized

---
+++ payment-executor/src/main/java/com/dps/payment_executor/PaymentExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.dps.payment_executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentExecutorApplication.class, args);
+	}
+
+}
